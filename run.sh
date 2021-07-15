bb663bf622033eeeb4462fc04f5c97c8f697eeac
Natalia Dubova <natalia.dubova@tidalmigrations.com>
2021-07-15 15:09:20 -0400

    Update due to feedback

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ -z $AWS_PROFILE ]; then
-  export AWS_PROFILE=default
-  echo "AWS_PROFILE not set. Using default"
-fi
-
-if [ -z $AWS_REGION ]; then
-  export AWS_REGION=ca-central-1
-  echo "AWS_REGION not set. Using default ca-central-1"
-fi
-
 # Compatible with AWS CLI v1.X
 function login() {
   login_cmd="$(aws ecr --profile $AWS_PROFILE get-login --no-include-email --region "$AWS_REGION")"
@@ -21,14 +11,6 @@ function login_v2() {
   registry="535558409775.dkr.ecr.ca-central-1.amazonaws.com"
   aws ecr --profile $AWS_PROFILE --region "$AWS_REGION" get-login-password | docker login --username AWS --password-stdin $registry
 }
-
-if [ "$AWS_CLI_V" = "1" ]; then
-  echo "Expecting to login using AWS CLI V1.X, if you have v2.X installed, set the env var 'AWS_CLI_V=2'."
-  login
-else
-  echo "Expecting to login using AWS CLI V2.X, if you have v1.X installed, set the env var 'AWS_CLI_V=1'."
-  login_v2
-fi
 
 # For use with local development
 function build(){
@@ -48,6 +30,24 @@ function deploy(){
     echo "Need to specify version tag such as vX where X is any number."
   fi
 }
+
+if [ -z $AWS_PROFILE ]; then
+  export AWS_PROFILE=default
+  echo "AWS_PROFILE not set. Using default"
+fi
+
+if [ -z $AWS_REGION ]; then
+  export AWS_REGION=ca-central-1
+  echo "AWS_REGION not set. Using default ca-central-1"
+fi
+
+if [ "$AWS_CLI_V" = "1" ]; then
+  echo "Expecting to login using AWS CLI V1.X, if you have v2.X installed, set the env var 'AWS_CLI_V=2'."
+  login
+else
+  echo "Expecting to login using AWS CLI V2.X, if you have v1.X installed, set the env var 'AWS_CLI_V=1'."
+  login_v2
+fi
 
 if [[ "$1" ]]; then
   "$@"
