8ff63393d2061818c1a24f3da9c6ce8dc83dd356
Natalia Dubova <natalia.dubova@tidalmigrations.com>
2021-07-15 12:34:13 -0400

    Update login

--- run.sh
+++ run.sh
@@ -10,16 +10,30 @@ if [ -z $AWS_REGION ]; then
   echo "AWS_REGION not set. Using default ca-central-1"
 fi
 
+# Compatible with AWS CLI v1.X
 function login() {
   login_cmd="$(aws ecr --profile $AWS_PROFILE get-login --no-include-email --region "$AWS_REGION")"
   eval "$login_cmd"
 }
 
+# Compatible with AWS CLI v2.X
+function login_v2() {
+  registry="535558409775.dkr.ecr.ca-central-1.amazonaws.com"
+  aws ecr --profile $AWS_PROFILE --region "$AWS_REGION" get-login-password | docker login --username AWS --password-stdin $registry
+}
+
+if [ "$AWS_CLI_V" = "1" ]; then
+  echo "Expecting to login using AWS CLI V1.X, if you have v2.X installed, set the env var 'AWS_CLI_V=2'."
+  login
+else
+  echo "Expecting to login using AWS CLI V2.X, if you have v1.X installed, set the env var 'AWS_CLI_V=1'."
+  login_v2
+fi
+
 # For use with local development
 function build(){
   if [[ "$1" ]]; then
     docker build -t "tidal/nightwatch:$1" .
-    exit
   else
     echo "Need to specify version tag such as vX where X is any number."
   fi
@@ -27,8 +41,7 @@ function build(){
 
 function deploy(){
   if [[ "$1" ]]; then
-    login
-    build
+    build "$@"
     docker tag tidal/nightwatch:$1 535558409775.dkr.ecr.ca-central-1.amazonaws.com/nightwatch:$1
     docker push 535558409775.dkr.ecr.ca-central-1.amazonaws.com/nightwatch:$1
   else
